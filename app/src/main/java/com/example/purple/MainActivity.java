3d267d8d38434fcdb6ab31523fe37e00e2829602
niiloliimatainen <niilo.liimatainen@student.lut.fi>
2020-03-09 13:11:00 +0200

    Purple V1.0

--- app/src/main/java/com/example/purple/MainActivity.java
+++ app/src/main/java/com/example/purple/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.purple;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void openActivity2(View v) {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+
+    }
+
+
+
 }
 
-//morjensta tässä oskari
+
 
