50c5c36b9172df899ea3d6e10f7f681a8f088829
Chef Kari <oskari.kivekas@student.lut.fi>
2020-03-12 17:59:33 +0200

    Purple V 1.0

--- app/src/main/java/com/example/purple/Activity3_2.java
+++ app/src/main/java/com/example/purple/Activity3_2.java
@@ -9,37 +9,34 @@ import android.content.Intent;
 import android.os.Bundle;
 
 
-public class Activity3 extends AppCompatActivity {
+public class Activity3_2 extends AppCompatActivity {
     private GestureDetectorCompat gesture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_3);
+        setContentView(R.layout.activity_activity3_2);
         gesture = new GestureDetectorCompat(this, new LearnGesture());
 
     }
+
     @Override
-    public boolean onTouchEvent(MotionEvent event){
+    public boolean onTouchEvent(MotionEvent event) {
         this.gesture.onTouchEvent(event);
         return super.onTouchEvent(event);
     }
 
     class LearnGesture extends GestureDetector.SimpleOnGestureListener {
         @Override
-        public boolean onFling(MotionEvent event1, MotionEvent event2, float x1, float y1){
-            if(event1.getX() > event2.getX()){
-                Intent intent1 = new Intent(Activity3.this, activity_3_1.class);
+        public boolean onFling(MotionEvent event1, MotionEvent event2, float x1, float y1) {
+            if (event2.getX() > event1.getX()) {
+                Intent intent1 = new Intent(Activity3_2.this, activity_3_1.class);
                 startActivity(intent1);
-            }else if(event2.getX() < event1.getX()){
+            } else if (event2.getX() < event1.getX()) {
 
             }
             return true;
         }
     }
-
-
-
-
 }
