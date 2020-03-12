aa6c8c276db53ba1d4f1c9c5e9c137cceff5218d
Chef Kari <oskari.kivekas@student.lut.fi>
2020-03-12 14:32:20 +0200

    Purple V 1.0

--- app/src/main/java/com/example/purple/Activity3.java
+++ app/src/main/java/com/example/purple/Activity3.java
@@ -9,6 +9,13 @@ public class Activity3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_3);
     }
+
+
+
+
+
+
 }
