ae28fa00d784ccef1ad378e8d1d765a97b59fa42
Chef Kari <oskari.kivekas@student.lut.fi>
2020-03-29 15:05:01 +0300

    Purple V 1.0

--- app/src/main/java/com/example/purple/Activity4.java
+++ app/src/main/java/com/example/purple/Activity4.java
@@ -14,7 +14,7 @@ import java.util.regex.Pattern;
 public class Activity4 extends AppCompatActivity {
     private EditText getFirstname, getLastname, getEmail, getPhoneNumber, getPassword, getPassword2;
     private String firstName, lastName, email, phoneNumber, password1, password2;
-
+    private boolean ok = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +30,7 @@ public class Activity4 extends AppCompatActivity {
     }
 
 
-    public void registeration(View v){
+    public void registeration(View v) {
         firstName = getFirstname.getText().toString();
         lastName = getLastname.getText().toString();
         email = getEmail.getText().toString();
@@ -38,42 +38,46 @@ public class Activity4 extends AppCompatActivity {
         password1 = getPassword.getText().toString();
 
         Pattern pattern1 = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
-        Pattern pattern2 = Pattern.compile("[^a-z0-9]");
-        Pattern pattern3 = Pattern.compile("[^A-Z]");
+        Pattern pattern2 = Pattern.compile("[a-z0-9]");
+        Pattern pattern3 = Pattern.compile("[A-Z]");
         Matcher matcher1 = pattern1.matcher(password1);
         Matcher matcher2 = pattern2.matcher(password1);
         Matcher matcher3 = pattern3.matcher(password1);
         boolean flag1 = matcher1.find();
         boolean flag2 = matcher2.find();
         boolean flag3 = matcher3.find();
+        System.out.println(flag1 +" " + flag2 +" "+ flag3);
 
-        if(password1.length() < 12){
-            Toast.makeText(getApplicationContext(), "Password has to be at least 12 characters!", Toast.LENGTH_LONG).show();
-        }else if(!flag1 && flag2 && flag3){
-            Toast.makeText(getApplicationContext(), "Password ok!", Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(getApplicationContext(), "Password has to contain number, small and capital character and special character!", Toast.LENGTH_LONG).show();
+        //tässä viel vähän säätämistä ja tarkistamista ton salasanan checkin kanssa//
+
+        while(true) {
+
+            if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || phoneNumber.isEmpty() || password1.isEmpty() || password2.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Fill all of the user information first!", Toast.LENGTH_LONG).show();
+            } else if (password1.length() < 12) {
+                Toast.makeText(getApplicationContext(), "Password has to be at least 12 characters!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Password has to contain number, small and capital character and special character!", Toast.LENGTH_LONG).show();
+            }
+            if (flag1 && flag2 && flag3) {
+                Toast.makeText(getApplicationContext(), "Password ok!", Toast.LENGTH_LONG).show();
+                ok = true;
+            } else {
+                ok = false;
+            }
+            password2 = getPassword2.getText().toString();
+            if (password2.equals(password1)) {
+                //tähän voisi laittaa sellasen hienon vihreen valon et näkee niitten salasanojen olevan ok//
+                ok = true;
+            } else {
+                Toast.makeText(getApplicationContext(), "Passwords don't match!", Toast.LENGTH_LONG).show();
+                ok = false;
+            }
+            if (ok) {
+                break;
+            }
         }
-
-        password2 = getPassword2.getText().toString();
-        if(password2.equals(password1)){
-            //tähän voisi laittaa sellasen hienon vihreen valon et näkee niitten salasanojen olevan ok//
-        }
-
-
-
-        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || phoneNumber.isEmpty() || password1.isEmpty() || password2.isEmpty()){
-            Toast.makeText(getApplicationContext(), "Fill all of the user information first!", Toast.LENGTH_LONG).show();
-
-        } else {
-            Intent intent = new Intent(this, Activity3.class);
-            startActivity(intent);
-        }
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
-;
-
-
-
-
-
 }
