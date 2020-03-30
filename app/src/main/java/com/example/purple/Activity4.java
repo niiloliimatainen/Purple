08cdfeecab88dcd21aac0440f782d20b887be20e
niiloliimatainen <niilo.liimatainen@student.lut.fi>
2020-03-30 20:02:03 +0300

    Purple V1.0

--- app/src/main/java/com/example/purple/Activity4.java
+++ app/src/main/java/com/example/purple/Activity4.java
@@ -37,28 +37,33 @@ public class Activity4 extends AppCompatActivity {
         email = getEmail.getText().toString();
         phoneNumber = getPhoneNumber.getText().toString();
         password1 = getPassword.getText().toString();
+        password2 = getPassword2.getText().toString();
 
         Pattern pattern1 = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
         Pattern pattern2 = Pattern.compile("[a-z0-9]");
         Pattern pattern3 = Pattern.compile("[A-Z]");
+        Pattern pattern4 = Pattern.compile("[a-z]");
         Matcher matcher1 = pattern1.matcher(password1);
         Matcher matcher2 = pattern2.matcher(password1);
         Matcher matcher3 = pattern3.matcher(password1);
+        Matcher matcher4 = pattern4.matcher(password1);
         boolean flag1 = matcher1.find();
         boolean flag2 = matcher2.find();
         boolean flag3 = matcher3.find();
-        System.out.println(flag1 +" " + flag2 +" "+ flag3);
+        boolean flag4 = matcher4.find();
+        System.out.println(flag1 +" " + flag2 +" "+ flag3 + " " + flag4);
 
         //tässä viel vähän säätämistä ja tarkistamista ton salasanan checkin kanssa//
         if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || phoneNumber.isEmpty() || password1.isEmpty() || password2.isEmpty()) {
             Toast.makeText(getApplicationContext(), "Fill all of the user information first!", Toast.LENGTH_LONG).show();
-        } else if (password1.length() < 12) {
+        } if (password1.length() < 12) {
+            System.out.println("Tuleeko=?????");
             Toast.makeText(getApplicationContext(), "Password has to be at least 12 characters!", Toast.LENGTH_LONG).show();
-        } else if (flag1 && flag2 && flag3) {
+        } else if (flag1 && flag2 && flag3 && flag4) {
             System.out.println("hä");
-            password2 = getPassword2.getText().toString();
             if (password2.equals(password1)) {
                 //tähän voisi laittaa sellasen hienon vihreen valon et näkee niitten salasanojen olevan ok//
+                //Ok aikasemmin
                 Toast.makeText(getApplicationContext(), "Password ok!", Toast.LENGTH_LONG).show();
                 ok = true;
             } else {
