9af15d50b794f1a4d40b4fa58a0a4adbb625db49
niiloliimatainen <niilo.liimatainen@student.lut.fi>
2020-03-30 18:00:17 +0300

    Purple V1.0

--- app/src/main/java/com/example/purple/databaseConnector.java
+++ app/src/main/java/com/example/purple/databaseConnector.java
@@ -1,7 +1,13 @@
 package com.example.purple;
 
-
 import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 //This is an utility class for file input and output.
 
@@ -11,13 +17,36 @@ public class databaseConnector {
     private databaseConnector() { }
 
     public static void writeToFile(User user) {
-        //jeejee
+        try {
+            FileWriter file = new FileWriter("database.json", true);
+            BufferedWriter writer = new BufferedWriter(file);
+            Gson gson = new Gson();
+            String json_object = gson.toJson(user);
+
+            writer.write(json_object);
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void readFromFile() {
-        //juujuuu
+
+    public static ArrayList<User> readFromFile() {
+        ArrayList<User> list = new ArrayList<User>();
+        Gson gson = new Gson();
+        try {
+            FileReader file = new FileReader("database.json");
+            BufferedReader reader = new BufferedReader(file);
+            String line = reader.readLine();
+            while (line != null) {
+                User user = gson.fromJson(line, User.class);
+                list.add(user);
+            }
+            reader.close();
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
-
-
-
 }
