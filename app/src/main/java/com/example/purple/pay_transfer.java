387013e78dd937f46703aba847f2f92c40442731
Chef Kari <oskari.kivekas@student.lut.fi>
2020-05-02 12:08:02 +0300

    Revert "Purple V 1.0"
    This reverts commit f67096834728c9ee71120c333bb49ca78a455d6c.

--- app/src/main/java/com/example/purple/pay_transfer.java
+++ app/src/main/java/com/example/purple/pay_transfer.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -70,7 +68,7 @@ public class pay_transfer extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void makeTransaction(){
+    public void makeTransaction(View v){
         Spinner chooseAcc = findViewById(R.id.chooseAcc);
         Spinner chooseAccToPay = findViewById(R.id.chooseAccToPay);
         chooseAcc.getSelectedItemPosition();
